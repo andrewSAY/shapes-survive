23c83fa1580c7194f1baf19a9a2184be71fc3be9
Saprykin.a <Saprykin.a@gitlab-dev.helix.ru>
2019-11-06 13:07:01 +0300

    Initial commit

---
+++ GUI.WinForms/IMainForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GUI.WinForms
+{
+    public interface IMainForm
+    {
+        void CreateNewBoard(int width, int height);
+    }
+}
